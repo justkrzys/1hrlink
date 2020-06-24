d504d5470615fc9eacfc3599fed74ecefe00b043
Krzysztof Gasienica-Bednarz <krzysztofgb@yahoo.com>
2020-06-24 18:05:08 -0500

    Added nodemon
    Added nodemon to allow for easier development of backend without needing to manually restart the server

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ let router = express.Router();
 /* GET home page. */
 router.get('/', (req, res) => {
   //Send test message to front-end
-  res.send({message: "I think this should work..."});
+  res.send({message: "It does work!"});
 });
 
 module.exports = router;
