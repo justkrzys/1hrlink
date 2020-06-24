21e2fe21078fa56da367d64695e8912ebc5eeb70
Krzysztof Gasienica-Bednarz <krzysztofgb@yahoo.com>
2020-06-23 22:28:59 -0500

    Connected frontend to backend
    Connected the frontend to the backend and cleaned up the file structure for the server-side. Preliminary sending of JSON data from server to client now works

--- server/app.js
+++ server/app.js
@@ -1,18 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let port = process.env.PORT || 5000;
 
-var app = express();
+let indexRouter = require('./routes/index');
+
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +39,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(port, () => console.log("Backend server live on " + port));
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', (req, res) => {
+  //Send test message to front-end
+  res.send({message: "I think this should work..."});
 });
 
 module.exports = router;
