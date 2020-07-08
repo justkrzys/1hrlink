95a8e999834795ffb60081b546ba3098e1581e7c
Pawel Rozanski <rozanski.pawel123@gmail.com>
2020-07-08 17:25:54 -0500

    Added a button and being able to type in box

--- client/src/LinkInput.js
+++ client/src/LinkInput.js
@@ -5,10 +5,22 @@ class LinkInput extends React.Component{
         link: ''
     }
 
+    onSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+        this.setState({
+            link:''
+        })
+    }
+
     render(){
         return(
             <form>
-                <input placeholder='Insert Link' value={this.state.link} />
+                <input placeholder='Insert Link'
+                       value={this.state.link}
+                       onChange={e => this.setState({ link: e.target.value})}
+                />
+                <button onClick={e => this.onSubmit(e)}>Submit</button>
             </form>
 
         );
